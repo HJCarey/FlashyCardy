f0783f8c5f8d823f34792fd9863c66b97000f4df
Jake <HJCarey@gmail.com>
2014-03-01 16:57:00 -0500

    Started work on file recall and Deck repopulation.

--- FlashyCardies/src/com/ramdev/flashycardies/Home.java
+++ FlashyCardies/src/com/ramdev/flashycardies/Home.java
@@ -1,7 +1,13 @@
 package com.ramdev.flashycardies;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
 import android.view.Menu;
 
 public class Home extends Activity {
@@ -9,6 +15,17 @@ public class Home extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+	
+		//Create master file for Deck names and saving functions
+		File masterList = new File("master.txt");
+		if(!masterList.exists()) {
+			try {
+				masterList.createNewFile();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}//end if
+		
 		setContentView(R.layout.activity_home);
 	}
 
@@ -17,6 +34,35 @@ public class Home extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.home, menu);
 		return true;
+	}
+	
+	public void populateDecks() {
+		File masterList = new File("master.txt");
+		String testString = "";
+		String delim = "[~]";
+		String[] deckNames;
+		
+		//Pull all text from masterList.txt
+		try {
+			FileInputStream fis = openFileInput(masterList.getName());
+			
+			byte[] dataArray = new byte[fis.available()];
+			while (fis.read(dataArray) != -1) {
+				testString = new String(dataArray);
+			}
+			fis.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		deckNames = testString.split(delim);
+		
+		for (int i=0; i<deckNames.length; i++) {
+			Deck deck = new Deck(deckNames[i]);
+			for (int j=0; i<)
+		}
 	}
 
 }
