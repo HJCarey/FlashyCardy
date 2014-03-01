b2cd3c19dc949e3112190685bb08f6afa34ae8a9
Ryan Murphy <murphy2902@gmail.com>
2014-03-01 16:55:34 -0500

    Testing EGit

--- FlashyCardies/src/com/ramdev/flashycardies/Home.java
+++ FlashyCardies/src/com/ramdev/flashycardies/Home.java
@@ -3,13 +3,55 @@ package com.ramdev.flashycardies;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class Home extends Activity {
+	
+	Button home_button_addDeck;
+	Button home_button_editDeckButton;
+	Button home_button_studyDeckButton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home);
+		
+		home_button_addDeck = (Button) findViewById(R.id.addDeck);
+		home_button_editDeckButton = (Button) findViewById(R.id.home_button_editDeckButton);
+		home_button_studyDeckButton = (Button) findViewById(R.id.home_button_studyDeckButton);
+		
+		initializeButtons();
+	}
+	
+	private void initializeButtons(){
+		home_button_addDeck.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
+		home_button_editDeckButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
+		home_button_studyDeckButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
 	}
 
 	@Override
