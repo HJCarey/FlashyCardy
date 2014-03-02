3d56ed49f59895fc012943b203bfa6fda8233878
Jake <HJCarey@gmail.com>
2014-03-02 09:17:02 -0500

    Added debug comments.

--- FlashyCardies/src/com/ramdev/flashycardies/Home.java
+++ FlashyCardies/src/com/ramdev/flashycardies/Home.java
@@ -11,7 +11,6 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
@@ -19,7 +18,6 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TableLayout;
-import android.widget.TextView;
 import android.widget.Toast;
 
 public class Home extends Activity {
